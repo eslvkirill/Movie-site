a54a2c21f02d1cdd0a6139cdb7dcef392d010914
Semyon Afanasyev <61320881+Befors@users.noreply.github.com>
2020-10-21 16:01:56 +0300

    add update and create operations for genre (#4)

--- src/main/java/com/movie/site/service/impl/GenreServiceImpl.java
+++ src/main/java/com/movie/site/service/impl/GenreServiceImpl.java
@@ -1,7 +1,11 @@
 package com.movie.site.service.impl;
 
+import com.movie.site.dto.request.CreateGenreDtoRequest;
+import com.movie.site.dto.request.UpdateGenreDtoRequest;
 import com.movie.site.dto.response.GenreDtoResponse;
+import com.movie.site.exception.GenreNotFoundException;
 import com.movie.site.mapper.GenreMapper;
+import com.movie.site.model.Genre;
 import com.movie.site.repository.GenreRepository;
 import com.movie.site.service.GenreService;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +23,24 @@ public class GenreServiceImpl implements GenreService {
     @Override
     public Collection<GenreDtoResponse> findAll() {
         return genreMapper.toDtoList(genreRepository.findAll());
+    }
+
+    @Override
+    public Genre create(CreateGenreDtoRequest genreDto) {
+        return genreRepository.save(genreMapper.toEntity(genreDto));
+    }
+
+    @Override
+    public Genre update(Long id, UpdateGenreDtoRequest genreDto) {
+        Genre genre = findById(id);
+
+        genreMapper.update(genreDto, genre);
+
+        return genreRepository.save(genre);
+    }
+
+    private Genre findById(Long id) {
+        return genreRepository.findById(id)
+                .orElseThrow(() -> new GenreNotFoundException(id));
     }
 }
