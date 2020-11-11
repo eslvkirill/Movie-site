455c459d85166cda2ccc59b564e6fdd6831625d3
semen afanasev <beforsss@yandex.ru>
2020-11-11 22:58:41 +0400

    add caching for the countries' api

--- src/main/java/com/movie/site/MovieSiteApplication.java
+++ src/main/java/com/movie/site/MovieSiteApplication.java
@@ -2,9 +2,11 @@ package com.movie.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 
 @SpringBootApplication(exclude = {ContextInstanceDataAutoConfiguration.class})
+@EnableCaching
 public class MovieSiteApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/movie/site/service/impl/CountryServiceImpl.java
+++ src/main/java/com/movie/site/service/impl/CountryServiceImpl.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.movie.site.service.CountryService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +14,7 @@ import java.util.*;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "countries")
 public class CountryServiceImpl implements CountryService {
 
     private final RestTemplate restClient;
@@ -23,6 +26,7 @@ public class CountryServiceImpl implements CountryService {
 
     @Override
     @SneakyThrows
+    @Cacheable
     public Collection<String> findAll() {
         String result = restClient.getForObject(COUNTRIES_API, String.class);
         JsonNode countriesNode = objectMapper.readTree(result);
